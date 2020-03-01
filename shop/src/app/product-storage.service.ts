24e7d56d0dd0cdc21ea4374dcb6bcd19ab557046
skaznik <skaznik@wszib.edu.pl>
2020-03-01 12:19:30 +0100

    angular-shop

---
+++ shop/src/app/product-storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {Product} from "./shop/products/Product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductStorageService {
+
+  constructor() { }
+
+  private products: Product[] = [
+    {id: 1, name: 'Produkt 1', price: 10.00, quantity: 1000, available: true},
+    {id: 2, name: 'Produkt 2', price: 15.00, quantity: 10, available: false}
+  ];
+}
